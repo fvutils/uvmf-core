c1d4e28664a95b2244f41c55961112876087a4f5
Matthew Ballance <matt.ballance@gmail.com>
2024-01-23 07:38:15 -0800

    Updated package structure
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

---
+++ setup.py
@@ -0,0 +1,37 @@
+
+import os, stat
+from setuptools import setup, find_namespace_packages
+
+version="0.0.1"
+
+if "BUILD_NUM" in os.environ.keys():
+    version += "." + os.environ["BUILD_NUM"]
+
+setup(
+  name = "uvmf-core",
+  version = version,
+  packages=find_namespace_packages(where='src'),
+  package_dir = {'' : 'src'},
+#  package_data = {'ivpm': ['scripts/*', 'templates/*', 'share/*', 'share/cmake/*']},
+  maintainer="Matthew Ballance",
+  maintainer_email="matt.ballance@gmail.com",
+  description = (""),
+  long_description="""
+  """,
+  license = "Apache 2.0",
+  keywords = ["SystemVerilog", "Verilog", "RTL", "Coverage"],
+  url = "https://github.com/fvutils/uvmf-core",
+  entry_points={
+    'console_scripts': [
+      'uvmf = uvmf.__main__:main'
+    ]
+  },
+  setup_requires=[
+    'setuptools_scm',
+  ],
+  install_requires=[
+      'voluptuous',
+      'jinja2'
+  ]
+)
+
